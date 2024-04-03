ab30f4a669ce40fefc08dac22b50e8f0c59b84a0
isamu <isamu@to-kyo.to>
2024-04-03 17:33:30 +0900

    anthropic

---
+++ src/llms/engines/anthropic.ts
@@ -0,0 +1,48 @@
+import { LlmUsage } from "../../types";
+
+import Manifest from "../../manifest";
+import FunctionCall from "../../function/function_call";
+
+import { LLMEngineBase } from "./base";
+import Anthropic, { ClientOptions } from "@anthropic-ai/sdk";
+import { ChatCompletionMessageParam } from "openai/resources/chat";
+
+export class LLMEngineAnthropic extends LLMEngineBase {
+  anthropic: Anthropic;
+
+  constructor(option?: ClientOptions) {
+    super();
+    this.anthropic = option ? new Anthropic(option) : new Anthropic();
+  }
+  async chat_completion(messages: ChatCompletionMessageParam[], manifest: Manifest, verbose: boolean) {
+    const functions = manifest.functions();
+    const function_call_param = manifest.function_call();
+    const model_name = manifest.model_name();
+
+    const send_message = messages
+      .filter((m) => {
+        return ["user", "system", "function", "assistant"].includes(m.role);
+      })
+      .map((a) => {
+        const { role, content } = a;
+        return { role: role === "system" ? "user" : role, content } as any;
+      });
+
+    console.log(send_message);
+    const chatCompletion = (await this.anthropic.messages.create({
+      max_tokens: 1024,
+      model: "claude-3-opus-20240229",
+      messages: [send_message[0]],
+    })) as any;
+
+    console.log(chatCompletion);
+    const res = chatCompletion.content[0].text;
+    const role = chatCompletion.role;
+    // const usage = chatCompletion.usage as LlmUsage;
+
+    // answer["function_call"] may be string, but actucally dict.
+    const function_call = null;
+
+    return { role, res, function_call, usage: null };
+  }
+}
