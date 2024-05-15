f99033252422ae45f1cabeb088d43146cc60570f
isamu <isamu@to-kyo.to>
2024-05-16 06:28:46 +0900

    update script

--- tests/run_manifest_image.ts
+++ tests/run_manifest_image.ts
@@ -1,7 +1,7 @@
 // put manifest file into manifests/main/
 // json and yaml ( or yml) support.
 // run script with open api key
-// OPENAI_API_KEY=sk-xxxxxx npx ts-node -r tsconfig-paths/register tests/run_manifest.ts business.json
+// OPENAI_API_KEY=sk-xxxxxx npx ts-node -r tsconfig-paths/register tests/run_manifest_image.ts image-gpt4.json
 
 import path from "path";
 import * as fs from "fs";
