3880d124c72ba74069743751204e3071c9a7b521
isamu <isamu@to-kyo.to>
2024-07-15 08:09:19 +0900

    fix lint errors

--- src/llms/engines/replicate.ts
+++ src/llms/engines/replicate.ts
@@ -1,7 +1,4 @@
-import { LlmUsage } from "@/types";
-
 import Manifest from "@/manifest";
-import FunctionCall from "@/function/function_call";
 
 import { LLMEngineBase } from "@/llms/engines/base";
 import { LlmModel } from "@/llms/model";
@@ -13,7 +10,7 @@ export class LLMEngineReplicate extends LLMEngineBase {
   replicate: Replicate;
   llm_models: LlmModel;
 
-  constructor(model: LlmModel, option?: any) {
+  constructor(model: LlmModel, __option?: any) {
     super();
     this.llm_models = model;
     const api_key = this.llm_models.get_api_key();
@@ -30,37 +27,39 @@ export class LLMEngineReplicate extends LLMEngineBase {
       .join("\n");
 
     const task = async (callback?: (message: string) => void) => {
-      return new Promise(async (resolved, reject) => {
-        try {
-          const prediction = await this.replicate.predictions.create({
-            model: model_name,
-            version: "2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1",
-            input: {
-              prompt: send_message,
-            },
-            stream: true,
-          });
-          let content: string[] = [];
-          if (prediction?.urls?.stream) {
-            const source = new EventSource(prediction.urls.stream, {
-              withCredentials: true,
+      return new Promise((resolved, reject) => {
+        (async () => {
+          try {
+            const prediction = await this.replicate.predictions.create({
+              model: model_name,
+              version: "2c1608e18606fad2812020dc541930f2d0495ce32eee50074220b87300bc16e1",
+              input: {
+                prompt: send_message,
+              },
+              stream: true,
             });
+            const content: string[] = [];
+            if (prediction?.urls?.stream) {
+              const source = new EventSource(prediction.urls.stream, {
+                withCredentials: true,
+              });
 
-            source.addEventListener("output", (e) => {
-              if (callback) {
-                callback(e.data);
-              }
-              content.push(e.data);
-            });
+              source.addEventListener("output", (e) => {
+                if (callback) {
+                  callback(e.data);
+                }
+                content.push(e.data);
+              });
 
-            source.addEventListener("done", (e) => {
-              source.close();
-              resolved(content.join(""));
-            });
+              source.addEventListener("done", (__e) => {
+                source.close();
+                resolved(content.join(""));
+              });
+            }
+          } catch (e) {
+            reject(e);
           }
-        } catch (e) {
-          reject(e);
-        }
+        })();
       });
     };
 
